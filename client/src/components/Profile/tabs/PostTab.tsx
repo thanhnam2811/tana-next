9bb9fd9ad8c316657b6c0883df5e05c1d049cc42
Thái Thành Nam <thanhnam.thai01@gmail.com>
2023-05-29 23:59:32 +0700

    use new list post in posttab at profile page

--- client/src/components/Profile/tabs/PostTab.tsx
+++ client/src/components/Profile/tabs/PostTab.tsx
@@ -1,10 +1,10 @@
-import { CreatePost, ListPost } from '@components/List';
 import React from 'react';
 import { Box, Stack } from '@mui/material';
 import { useInfiniteFetcher } from '@hooks';
 import { PictureContainer } from '../PictureContainer';
 import { CenterArea } from '@layout';
 import { useUserStore } from '@store';
+import { CreatePost, ListPost } from '@components/v2/List';
 
 interface Props {
 	user: any;
