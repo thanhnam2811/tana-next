45e5c11348e7263772e47407e311f89c990ba8f4
Cao Hoài Tấn <caohoaitan@gmail.com>
2023-06-02 23:45:00 +0700

    add gender when register

--- server/src/app/controllers/AccessController.js
+++ server/src/app/controllers/AccessController.js
@@ -2,69 +2,96 @@ const Access = require('../models/Access');
 const moment = require('moment');
 
 class AccessController {
-    //Update access of user in day when user connect on socket
-    async updateAccessInDay() {
-        try {
-            const access = await Access.findOne({
-                day: {
-                    $gte: moment().startOf('day').toDate(), // start of day
-                    $lte: moment().endOf('day').toDate(), // end of day
-                },
-            });
+	//Update access of user in day when user connect on socket
+	async updateAccessInDay() {
+		try {
+			const access = await Access.findOne({
+				day: {
+					$gte: moment().startOf('day').toDate(), // start of day
+					$lte: moment().endOf('day').toDate(), // end of day
+				},
+			});
 
-            if (access) {
-                access.totalAccess += 1;
-                await access.save();
-            } else {
-                const newAccess = new Access({
-                    totalAccess: 1,
-                });
-                await newAccess.save();
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    }
+			if (access) {
+				access.totalAccess += 1;
+				await access.save();
+			} else {
+				const newAccess = new Access({
+					totalAccess: 1,
+				});
+				await newAccess.save();
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-    async dailyAccessSevenDaysAgo() {
-        try {
-            //get Daily access 7 days ago
-            const access = await Access.find({
-                day: {
-                    $gte: moment().subtract(7, 'days').startOf('day').toDate(), // start of 7 days ago
-                    $lte: moment().subtract(1, 'days').endOf('day').toDate(), // end of 1 day ago
-                },
-                sort: { day: -1 },
-            });
+	async dailyAccessSevenDaysAgo(startDay, endDay) {
+		try {
+			const totalAccessByDay = await Access.aggregate([
+				{
+					$match: {
+						day: {
+							$gte: new Date(startDay),
+							$lte: new Date(endDay),
+						},
+					},
+				},
+				{
+					$group: {
+						_id: { $dateToString: { format: '%Y-%m-%d', date: '$day' } },
+						totalAccess: { $sum: '$totalAccess' },
+					},
+				},
+				{
+					$sort: { _id: 1 },
+				},
+			]);
 
-            const dailyAccess = [];
-            for (let i = 0; i < 7; i++) {
-                const day = moment().subtract(7 - i, 'days').toDate(); // get day i days ago
-                dailyAccess.push({
-                    day: day,
-                    totalAccess: access[i] ? access[i].totalAccess : 0,
-                });
-            }
+			// Create an object to store the total access by day
+			const totalAccessMap = {};
 
-            return dailyAccess;
-        } catch (error) {
-            console.log(error);
-        }
-    }
+			// Initialize the map with 0 for each day in the range
+			let currentDate = new Date(startDay);
+			const endDate = new Date(endDay);
+			while (currentDate <= endDate) {
+				const dateString = currentDate.toISOString().split('T')[0];
+				totalAccessMap[dateString] = 0;
 
-    async getNumAccessInDay() {
-        try {
-            const access = await Access.findOne({
-                day: {
-                    $gte: moment().startOf('day').toDate(), // start of day
-                    $lte: moment().endOf('day').toDate(), // end of day
-                },
-            });
-            return access ? access.totalAccess : 0;
-        } catch (error) {
-            console.log(error);
-        }
-    }
+				const nextDate = currentDate.setDate(currentDate.getDate() + 1);
+				currentDate = new Date(nextDate);
+			}
+
+			// Update the map with the actual total access
+			totalAccessByDay.forEach((result) => {
+				totalAccessMap[result._id] = result.totalAccess;
+			});
+
+			// Convert the map into an array of objects
+			const totalAccessByDayArray = Object.keys(totalAccessMap).map((dateString) => ({
+				day: dateString,
+				totalAccess: totalAccessMap[dateString],
+			}));
+
+			return totalAccessByDayArray;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	async getNumAccessInDay() {
+		try {
+			const access = await Access.findOne({
+				day: {
+					$gte: moment().startOf('day').toDate(), // start of day
+					$lte: moment().endOf('day').toDate(), // end of day
+				},
+			});
+			return access ? access.totalAccess : 0;
+		} catch (error) {
+			console.log(error);
+		}
+	}
 }
 
 module.exports = new AccessController();
