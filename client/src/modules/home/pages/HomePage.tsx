30f7066fdf02ab6b751998d3aea4633a6d69f8d9
Thái Thành Nam <thanhnam.thai01@gmail.com>
2023-06-04 16:34:31 +0700

    done page home

---
+++ client/src/modules/home/pages/HomePage.tsx
@@ -0,0 +1,30 @@
+import { useInfiniteFetcherSWR } from '@hooks';
+import Layout, { withLayout } from '@layout';
+import { withAuth } from '@modules/auth/components';
+import { CreatePost, ListPost } from '@modules/post/components';
+import { PostType } from '@modules/post/types';
+import { QuickContact, ShortCut } from '../components';
+
+function HomePage() {
+	const postFetch = useInfiniteFetcherSWR<PostType>({ api: 'posts/home' });
+
+	return (
+		<>
+			<Layout.Sider align="left">
+				<ShortCut />
+			</Layout.Sider>
+
+			<Layout.Content>
+				<CreatePost fetcher={postFetch} style={{ marginBottom: 16 }} />
+
+				<ListPost fetcher={postFetch} />
+			</Layout.Content>
+
+			<Layout.Sider align="right">
+				<QuickContact />
+			</Layout.Sider>
+		</>
+	);
+}
+
+export default withAuth(withLayout(HomePage));
