40f39400d78cbbd4594cc81ee0ad4a245118af9c
Cao Hoài Tấn <caohoaitan@gmail.com>
2023-06-06 14:57:28 +0700

    notification

--- server/src/app/models/Notification.js
+++ server/src/app/models/Notification.js
@@ -1,58 +1,60 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const mongooseDelete = require('mongoose-delete');
 const mongoosePaginate = require('mongoose-paginate-v2');
 
+const read_by = mongoose.Schema(
+	{
+		readerId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+		},
+		readAt: {
+			type: Date,
+			default: Date.now,
+		},
+	},
+	{ _id: false }
+);
 
 const NotificationSchema = new mongoose.Schema(
-    {
-        type: {
-            type: String,
-            required: true
-        },
-        content: {
-            type: String,
-            required: true
-        },
-        //link to object create notification
-        link: {
-            type: String,
-            required: true,
-        },
-        sender: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-            required: true
-        },
-        receiver: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'User',
-                required: true
-            }
-        ],
-        read_by: [
-            {
-                readerId: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: 'User',
-                },
-                readAt: {
-                    type: Date,
-                    default: Date.now
-                }
-            }
-        ]
-    },
-    { timestamps: true }
+	{
+		type: {
+			type: String,
+			required: true,
+		},
+		content: {
+			type: String,
+			required: true,
+		},
+		//link to object create notification
+		link: {
+			type: String,
+			required: true,
+		},
+		sender: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		receiver: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'User',
+				required: true,
+			},
+		],
+		read_by: [read_by],
+	},
+	{ timestamps: true }
 );
 
 //soft delete
 NotificationSchema.plugin(mongooseDelete, {
-    deletedAt: true,
-    overrideMethods: 'all',
+	deletedAt: true,
+	overrideMethods: 'all',
 });
 
 //paginate
 NotificationSchema.plugin(mongoosePaginate);
 
-module.exports = mongoose.model("Notification", NotificationSchema);
+module.exports = mongoose.model('Notification', NotificationSchema);
