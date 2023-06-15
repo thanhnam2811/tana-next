39bdd2f1f9427489137656ff3edb8f536fbc3810
Thái Thành Nam <thanhnam.thai01@gmail.com>
2023-06-15 15:44:21 +0700

    update SEO

--- client/src/pages/post/[id].tsx
+++ client/src/pages/post/[id].tsx
@@ -1,68 +1,37 @@
-import { stringUtil, urlUtil } from '@common/utils';
 import { PostType } from '@common/types';
 import { getPostApi } from '@modules/post/api';
 import PostPage from '@modules/post/pages/PostPage';
 import axios from 'axios';
 import { GetServerSideProps } from 'next';
-import Head from 'next/head';
+import { PostSEO } from '@modules/post/components';
 
 interface Props {
 	post?: PostType;
+	id?: string;
 }
 
 export const getServerSideProps: GetServerSideProps<Props> = async ({ params }) => {
+	const id = params?.id as string;
+	const props: Props = { id };
 	try {
-		const id = params?.id as string;
-
 		const post = await getPostApi(id, true);
 
-		return { props: { post } };
+		props.post = post;
 	} catch (error) {
-		console.log(error);
 		if (axios.isAxiosError(error)) {
 			if (error.response?.status === 404) {
 				return { notFound: true };
 			}
 		}
-
-		return { props: {} };
 	}
+
+	return { props };
 };
 
-export default function Post({ post }: Props) {
-	const link = urlUtil.getFullUrl(`/post/${post?._id}`);
-
-	let title = 'Bạn cần đăng nhập để xem bài viết này';
-	let description = 'Đây là bài viết không công khai. Để xem bài viết này, bạn cần đăng nhập.';
-	if (post) {
-		const author = post.author.fullname;
-		title = `Bài viết của ${author}`;
-
-		const text = stringUtil.htmlToPlainText(post.content);
-		description = text;
-	}
-
-	let pictureUrl = urlUtil.getFullUrl('/logo.png');
-	if (post?.media[0]?.link) {
-		pictureUrl = post?.media[0]?.link;
-	}
-
+export default function Post({ post, id }: Props) {
 	return (
 		<>
-			<Head>
-				<meta name="description" content={post?.content} />
-				<link rel="canonical" href={`/post/${post?._id}`} />
-
-				{/* Social media meta tags */}
-				<meta property="og:type" content="website" />
-				<meta property="og:title" content={title} />
-				<meta property="og:description" content={description} />
-				<meta property="og:url" content={link} />
-				<meta property="og:image" content={pictureUrl} />
-
-				{/* Title */}
-				<title>{title}</title>
-			</Head>
+			<PostSEO id={id} post={post} />
 
 			<PostPage post={post} />
 		</>
