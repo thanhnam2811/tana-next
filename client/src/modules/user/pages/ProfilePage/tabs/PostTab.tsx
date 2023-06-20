6645e08438df35592049a5e264299a847740ec00
Thái Thành Nam <thanhnam.thai01@gmail.com>
2023-06-20 09:50:28 +0700

    user action

--- client/src/modules/user/pages/ProfilePage/tabs/PostTab.tsx
+++ client/src/modules/user/pages/ProfilePage/tabs/PostTab.tsx
@@ -3,13 +3,16 @@ import { PostType } from '@common/types';
 import { CreatePost, ListPost } from '@modules/post/components';
 import { useUserContext } from '@modules/user/hooks';
 import styles from './PostTab.module.scss';
-import { Button, Image, Tooltip, Typography, theme } from 'antd';
+import { Button, Image, Tooltip, Typography, theme, Dropdown, Popconfirm } from 'antd';
 import { HiCamera, HiUserPlus } from 'react-icons/hi2';
 import { UploadImage } from '@common/components/Button';
 import { useAuth } from '@modules/auth/hooks';
 import { toast } from 'react-hot-toast';
 import { uploadFileApi } from '@common/api';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
+import { requestFriendApi, unFriendApi } from '@modules/friend/api';
+import { createConversationApi } from '@modules/messages/api';
+import { useRouter } from 'next/router';
 
 export function PostTab() {
 	const { token } = theme.useToken();
@@ -49,40 +52,186 @@ export function PostTab() {
 		}
 	};
 
+	const [relationship, setRelationship] = useState(user.relationship);
+	const [loading, setLoadingState] = useState<{ [key: string]: boolean }>({});
+	const setLoading = (key: string, value: boolean) => setLoadingState((prev) => ({ ...prev, [key]: value }));
+
+	const handleRequestFriend = async () => {
+		const toastId = toast.loading('Đang gửi lời mời kết bạn...');
+		setLoading('request', true);
+
+		try {
+			await requestFriendApi(user._id);
+			toast.success('Gửi lời mời kết bạn thành công!', { id: toastId });
+			setRelationship('sent');
+		} catch (error: any) {
+			toast.error(error.message || error.toString(), { id: toastId });
+		}
+
+		setLoading('request', false);
+	};
+
+	const handleUnfriend = async () => {
+		const toastId = toast.loading('Đang hủy kết bạn...');
+		setLoading('unfriend', true);
+
+		try {
+			await unFriendApi(user._id);
+			toast.success('Hủy kết bạn thành công!', { id: toastId });
+			setRelationship('none');
+		} catch (error: any) {
+			toast.error(error.message || error.toString(), { id: toastId });
+		}
+
+		setLoading('unfriend', false);
+	};
+
+	const router = useRouter();
+	const handleChat = async () => {
+		const toastId = toast.loading('Đang chuyển hướng đến trang nhắn tin...');
+		setLoading('chat', true);
+
+		try {
+			const created = await createConversationApi({ members: [{ user: user._id }] });
+			await router.push(`/messages?id=${created._id}`);
+
+			toast.dismiss(toastId);
+		} catch (error) {
+			toast('Có lỗi xảy ra, vui lòng thử lại sau', { id: toastId });
+		}
+
+		setLoading('chat', false);
+	};
+
+	const handleAcceptFriend = async () => {
+		const toastId = toast.loading('Đang xác nhận lời mời kết bạn...');
+		setLoading('accept', true);
+
+		try {
+			await requestFriendApi(user._id);
+			toast.success('Xác nhận lời mời kết bạn thành công! Bạn bè với nhau rồi đó!', { id: toastId });
+			setRelationship('friend');
+		} catch (error: any) {
+			toast.error(error.message || error.toString(), { id: toastId });
+		}
+
+		setLoading('accept', false);
+	};
+
+	const handleRejectFriend = async () => {
+		const toastId = toast.loading('Đang từ chối lời mời kết bạn...');
+		setLoading('reject', true);
+
+		try {
+			await requestFriendApi(user._id);
+			toast.success('Từ chối lời mời kết bạn thành công!', { id: toastId });
+			setRelationship('none');
+		} catch (error: any) {
+			toast.error(error.message || error.toString(), { id: toastId });
+		}
+
+		setLoading('reject', false);
+	};
+
+	const handleCancelRequestFriend = async () => {
+		const toastId = toast.loading('Đang hủy lời mời kết bạn...');
+		setLoading('cancel', true);
+
+		try {
+			await requestFriendApi(user._id); // Request friend again to cancel
+			toast.success('Hủy lời mời kết bạn thành công!', { id: toastId });
+			setRelationship('none');
+		} catch (error: any) {
+			toast.error(error.message || error.toString(), { id: toastId });
+		}
+
+		setLoading('cancel', false);
+	};
+
 	const actions: ReactNode[] = [];
 
 	if (!isCurrentUser) {
-		switch (user.relationship) {
+		switch (relationship) {
 			case 'none':
 				actions.push(
-					<Button key="add-friend" type="primary" icon={<HiUserPlus />} onClick={console.log}>
+					<Button
+						key="add-friend"
+						type="primary"
+						icon={<HiUserPlus />}
+						onClick={handleRequestFriend}
+						loading={loading.request}
+					>
 						Kết bạn
 					</Button>
 				);
 				break;
 			case 'friend':
 				actions.push(
-					<Button key="unfriend" type="primary" onClick={console.log}>
-						Bạn bè
-					</Button>,
-					<Button key="chat" onClick={console.log}>
+					<Dropdown
+						key="friend"
+						arrow
+						trigger={['click']}
+						menu={{
+							items: [
+								{
+									key: 'unfriend',
+									label: (
+										<Popconfirm
+											title="Bạn có chắc muốn hủy kết bạn?"
+											okText="Đồng ý"
+											cancelText="Hủy"
+											onConfirm={handleUnfriend}
+										>
+											Hủy kết bạn
+										</Popconfirm>
+									),
+									danger: true,
+								},
+							],
+						}}
+					>
+						<Button type="primary">Bạn bè</Button>
+					</Dropdown>,
+					<Button key="chat" onClick={handleChat} loading={loading.chat}>
 						Nhắn tin
 					</Button>
 				);
 				break;
 			case 'sent':
 				actions.push(
-					<Button key="cancel-request" onClick={console.log}>
-						Đã gửi lời mời
-					</Button>
+					<Dropdown
+						key="sent"
+						arrow
+						trigger={['click']}
+						menu={{
+							items: [
+								{
+									key: 'cancel',
+									label: (
+										<Popconfirm
+											title="Bạn có chắc muốn hủy lời mời kết bạn?"
+											okText="Đồng ý"
+											cancelText="Hủy"
+											onConfirm={handleCancelRequestFriend}
+										>
+											Hủy lời mời
+										</Popconfirm>
+									),
+									danger: true,
+								},
+							],
+						}}
+					>
+						<Button type="primary">Đã gửi lời mời</Button>
+					</Dropdown>
 				);
 				break;
 			case 'received':
 				actions.push(
-					<Button key="accept-request" type="primary" onClick={console.log}>
+					<Button key="accept" type="primary" onClick={handleAcceptFriend} loading={loading.accept}>
 						Chấp nhận
 					</Button>,
-					<Button key="decline-request" onClick={console.log} danger>
+					<Button key="reject" danger onClick={handleRejectFriend} loading={loading.reject}>
 						Từ chối
 					</Button>
 				);
@@ -91,8 +240,6 @@ export function PostTab() {
 				break;
 		}
 	}
-
-	console.log({ user, actions });
 
 	return (
 		<>
