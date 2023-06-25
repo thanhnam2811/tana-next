c523482c3686b98853a9b635fa994f9cc0d38c9e
Thái Thành Nam <thanhnam.thai01@gmail.com>
2023-06-25 22:48:48 +0700

    approve, reject report

---
+++ admin/src/modules/report/api/index.ts
@@ -0,0 +1,2 @@
+export * from './reject-report.api.ts';
+export * from './approve-report.api.ts';
--- admin/src/modules/report/pages/ReportDetail.tsx
+++ admin/src/modules/report/pages/ReportDetail.tsx
@@ -3,15 +3,19 @@ import useSWR from 'swr';
 import { swrFetcher } from '@common/api';
 import { FullscreenSpin } from '@common/components/Loading';
 import { ReportType } from '@modules/report/types';
-import { Card, Image, Typography } from 'antd';
+import { Button, Card, Image, message, Space, Typography } from 'antd';
 import { timeUtil } from '@common/utils';
 import { StatusTag } from '@modules/report/components';
+import { approveReportApi } from '@modules/report/api';
+import { useState } from 'react';
 
 function ReportDetail() {
 	const { id } = useParams();
-	const { data: report, isLoading, error } = useSWR<ReportType, string>(`/reports/${id}`, swrFetcher);
-
-	console.log({ report, isLoading, error });
+	const { data: report, isLoading, error, mutate } = useSWR<ReportType, string>(`/reports/${id}`, swrFetcher);
+	const [processing, setProcessing] = useState({
+		approve: false,
+		reject: false,
+	});
 
 	if (isLoading) return <FullscreenSpin />;
 
@@ -19,10 +23,73 @@ function ReportDetail() {
 
 	if (!report) return <Navigate to="/404" />;
 
+	const handleApprove = async () => {
+		setProcessing({ ...processing, approve: true });
+		const key = 'approve';
+		message.loading({ content: 'Đang duyệt báo cáo...', key });
+
+		try {
+			await approveReportApi(report._id);
+
+			await mutate({ ...report, status: 'approved' }, false);
+
+			message.success({ content: 'Duyệt báo cáo thành công!', key });
+		} catch (error) {
+			message.error({ content: `Duyệt báo cáo thất bại! ${error}`, key });
+		}
+
+		setProcessing({ ...processing, approve: false });
+	};
+
+	const handleReject = async () => {
+		setProcessing({ ...processing, reject: true });
+		const key = 'reject';
+		message.loading({ content: 'Đang từ chối báo cáo...', key });
+
+		try {
+			await approveReportApi(report._id);
+
+			await mutate({ ...report, status: 'rejected' }, false);
+
+			message.success({ content: 'Từ chối báo cáo thành công!', key });
+		} catch (error) {
+			message.error({ content: `Từ chối báo cáo thất bại! ${error}`, key });
+		}
+
+		setProcessing({ ...processing, reject: false });
+	};
+
 	const { reporter, images } = report;
 
 	return (
-		<Card title="Chi tiết báo cáo" loading={isLoading}>
+		<Card
+			title="Chi tiết báo cáo"
+			loading={isLoading}
+			extra={
+				report.status === 'pending' && (
+					<Space split>
+						<Button
+							type="primary"
+							danger
+							onClick={handleReject}
+							loading={processing.reject}
+							disabled={processing.approve}
+						>
+							Từ chối
+						</Button>
+
+						<Button
+							type="primary"
+							onClick={handleApprove}
+							loading={processing.approve}
+							disabled={processing.reject}
+						>
+							Duyệt
+						</Button>
+					</Space>
+				)
+			}
+		>
 			<Typography>
 				<Typography.Title level={5}>
 					<b>Tiêu đề:</b> {report.title}
@@ -51,9 +118,18 @@ function ReportDetail() {
 
 					{images && images?.length > 0 ? (
 						<Image.PreviewGroup>
-							{report.images?.map((image) => (
-								<Image key={image._id} width={100} height={100} src={image.link} />
-							))}
+							<Space>
+								{report.images?.map((image, index) => (
+									<Image
+										key={image._id}
+										width={100}
+										height={100}
+										src={image.link}
+										alt={`Ảnh đính kèm ${index + 1}`}
+										style={{ objectFit: 'cover' }}
+									/>
+								))}
+							</Space>
 						</Image.PreviewGroup>
 					) : (
 						<Typography.Text type="secondary">(Không có hình ảnh)</Typography.Text>
