d592ba5293df73c115d3c05b42533ef13387315b
Thái Thành Nam <thanhnam.thai01@gmail.com>
2023-07-12 13:25:51 +0700

    turn off post SEO

--- client/src/pages/post/[id].tsx
+++ client/src/pages/post/[id].tsx
@@ -10,25 +10,27 @@ interface Props {
 	error?: object;
 }
 
-export const getServerSideProps: GetServerSideProps<Props> = async ({ params }) => {
-	const id = params?.id as string;
-	const props: Props = { id };
-	try {
-		props.post = await getPostApi(id, true);
-	} catch (e: any) {
-		if (ApiError.isApiError(e)) props.error = e.toObject();
-		else props.error = ApiError.fromError(e).toObject();
-	}
+// export const getServerSideProps: GetServerSideProps<Props> = async ({ params }) => {
+// 	const id = params?.id as string;
+// 	const props: Props = { id };
+// 	try {
+// 		props.post = await getPostApi(id, true);
+// 	} catch (e: any) {
+// 		if (ApiError.isApiError(e)) props.error = e.toObject();
+// 		else props.error = ApiError.fromError(e).toObject();
+// 	}
+//
+// 	return { props };
+// };
 
-	return { props };
-};
+// export default function Post({ post, id, error }: Props) {
+// 	return (
+// 		<>
+// 			<PostSEO id={id} post={post} error={error && ApiError.fromObject(error)} />
+//
+// 			<PostPage post={post} />
+// 		</>
+// 	);
+// }
 
-export default function Post({ post, id, error }: Props) {
-	return (
-		<>
-			<PostSEO id={id} post={post} error={error && ApiError.fromObject(error)} />
-
-			<PostPage post={post} />
-		</>
-	);
-}
+export default PostPage;
